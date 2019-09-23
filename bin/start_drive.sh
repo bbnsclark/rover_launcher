a82962476377913a41f6639f78cc48fdb6e1ea42
Giovani Del Nero Diniz <gdiniz@bbn.com>
2019-09-23 11:36:23 -0400

    cleanup file paths

--- bin/start_drive.sh
+++ bin/start_drive.sh
@@ -1,11 +1,11 @@
 #! /bin/bash
 
 [ -f "/opt/ros/melodic/setup.sh" ] && source "/opt/ros/melodic/setup.sh"
-[ -f "/home/nvidia/rover/devel/setup.sh" ] && source "/home/nvidia/rover/devel/setup.sh"
+[ -f "$HOME/rover/devel/setup.sh" ] && source "$HOME/rover/devel/setup.sh"
 #[ -f "/etc/default/ros" ] && source "/etc/default/ros"
 
 set -a
-source /home/nvidia/rover/src/rover_launcher/config/CCAST_CONFIG
+source $HOME/rover/src/rover_launcher/config/CCAST_CONFIG
 
 PYTHONPATH="$PYTHONPATH:/opt/ros/melodic/share/roboclaw_node/src/roboclaw_driver"
-roslaunch /home/nvidia/rover/src/rover_launcher/launch/drive.launch
+roslaunch $HOME/rover/src/rover_launcher/launch/drive.launch
