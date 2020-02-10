9b1a2d976f515e4ef195dbb9f440a9aac72fbf02
Giovani Del Nero Diniz <gdiniz@bbn.com>
2020-02-10 15:07:22 -0500

    [FIX] included april tag detection

--- bin/start.sh
+++ bin/start.sh
@@ -1,41 +1,45 @@
 #!/bin/bash
 
-/home/nvidia/rover/src/rover_launcher/bin/start_rosclean.sh
+$HOME/rover/src/rover_launcher/bin/start_rosclean.sh
 
 sleep 1
 
-screen -d -m -S roscore /home/nvidia/rover/src/rover_launcher/bin/start_roscore.sh
+screen -d -m -S roscore $HOME/rover/src/rover_launcher/bin/start_roscore.sh
 
 sleep 5
 
-screen -d -m -S rosbridge /home/nvidia/rover/src/rover_launcher/bin/start_rosbridge.sh
+screen -d -m -S rosbridge $HOME/rover/src/rover_launcher/bin/start_rosbridge.sh
 
 sleep 5
 
-screen -d -m -S realsense /home/nvidia/rover/src/rover_launcher/bin/start_realsense.sh
+screen -d -m -S realsense $HOME/rover/src/rover_launcher/bin/start_realsense.sh
 
 sleep 5
 
-screen -d -m -S imu /home/nvidia/rover/src/rover_launcher/bin/start_imu.sh
+screen -d -m -S april_tags $HOME/rover/src/rover_launcher/bin/start_tag_detection.sh
 
 sleep 5
 
-screen -d -m -S drive /home/nvidia/rover/src/rover_launcher/bin/start_drive.sh
+screen -d -m -S imu $HOME/rover/src/rover_launcher/bin/start_imu.sh
 
 sleep 5
 
-screen -d -m -S lidar /home/nvidia/rover/src/rover_launcher/bin/start_lidar.sh
+screen -d -m -S drive $HOME/rover/src/rover_launcher/bin/start_drive.sh
 
 sleep 5
 
-screen -d -m -S ekf_inertial /home/nvidia/rover/src/rover_launcher/bin/start_ekf_inertial.sh
+screen -d -m -S lidar $HOME/rover/src/rover_launcher/bin/start_lidar.sh
 
 sleep 5
 
-screen -d -m -S video /home/nvidia/rover/src/rover_launcher/bin/start_video_server.sh
+screen -d -m -S ekf_inertial $HOME/rover/src/rover_launcher/bin/start_ekf_inertial.sh
 
 sleep 5
 
-screen -d -m -S state_obs /home/nvidia/rover/src/rover_launcher/bin/start_state_observer.sh
+screen -d -m -S video $HOME/rover/src/rover_launcher/bin/start_video_server.sh
+
+sleep 5
+
+screen -d -m -S state_obs $HOME/rover/src/rover_launcher/bin/start_state_observer.sh
 
 sleep 5
