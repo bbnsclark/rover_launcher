6ead85435a8fc03f6908680fab2fa2eaeadd4114
Giovani Del Nero Diniz <gdiniz@bbn.com>
2020-02-10 17:35:30 -0500

    [FIX] echoing some output at startup

--- bin/start.sh
+++ bin/start.sh
@@ -4,25 +4,33 @@ $HOME/rover/src/rover_launcher/bin/start_rosclean.sh
 
 sleep 1
 
+echo 'Starting nodes:'
+echo '    - roscore'
+
 screen -d -m -S roscore $HOME/rover/src/rover_launcher/bin/start_roscore.sh
 
 sleep 5
+echo '    - rosbridge'
 
 screen -d -m -S rosbridge $HOME/rover/src/rover_launcher/bin/start_rosbridge.sh
 
 sleep 5
+echo '    - realsense'
 
 screen -d -m -S realsense $HOME/rover/src/rover_launcher/bin/start_realsense.sh
 
 sleep 5
+echo '    - april tag detections'
 
 screen -d -m -S april_tags $HOME/rover/src/rover_launcher/bin/start_tag_detection.sh
 
 sleep 5
+echo '    - imu'
 
 screen -d -m -S imu $HOME/rover/src/rover_launcher/bin/start_imu.sh
 
 sleep 5
+echo '    - drive system'
 
 screen -d -m -S drive $HOME/rover/src/rover_launcher/bin/start_drive.sh
 
