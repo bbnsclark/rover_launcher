10bc5e87c8bc75742f9c9480e4796b37aec74052
Giovani Del Nero Diniz <gdiniz@bbn.com>
2020-02-11 07:27:57 -0500

    [TEST] speed up startup process

--- bin/start.sh
+++ bin/start.sh
@@ -9,59 +9,59 @@ echo '    - roscore'
 
 screen -d -m -S roscore $HOME/rover/src/rover_launcher/bin/start_roscore.sh
 
-sleep 5
+sleep 2
 echo '    - rosbridge'
 
 screen -d -m -S rosbridge $HOME/rover/src/rover_launcher/bin/start_rosbridge.sh
 
-sleep 5
+sleep 2
 echo '    - realsense'
 
 screen -d -m -S realsense $HOME/rover/src/rover_launcher/bin/start_realsense.sh
 
-sleep 5
+sleep 2
 echo '    - april tag detections'
 
 screen -d -m -S april_tags $HOME/rover/src/rover_launcher/bin/start_tag_detection.sh
 
-sleep 5
+sleep 2
 echo '    - imu'
 
 screen -d -m -S imu $HOME/rover/src/rover_launcher/bin/start_imu.sh
 
-sleep 5
+sleep 2
 echo '    - drive system'
 
 screen -d -m -S drive $HOME/rover/src/rover_launcher/bin/start_drive.sh
 
-sleep 5
+sleep 2
 echo '    - lidar'
 
 screen -d -m -S lidar $HOME/rover/src/rover_launcher/bin/start_lidar.sh
 
-sleep 5
+sleep 2
 echo '    - kalman filter'
 
 screen -d -m -S ekf_inertial $HOME/rover/src/rover_launcher/bin/start_ekf_inertial.sh
 
-sleep 5
+sleep 2
 echo '    - video stream'
 
 screen -d -m -S video $HOME/rover/src/rover_launcher/bin/start_video_server.sh
 
-sleep 5
+sleep 2
 echo '    - obstacle avoidance'
 
 screen -d -m -S avoidance $HOME/rover/src/rover_launcher/bin/start_avoidance.sh
 
-sleep 5
+sleep 2
 echo '    - navigation'
 
 screen -d -m -S navigation $HOME/rover/src/rover_launcher/bin/start_nav.sh
 
-sleep 5
+sleep 2
 echo '    - state observer'
 
 screen -d -m -S state_obs $HOME/rover/src/rover_launcher/bin/start_state_observer.sh
 
-sleep 5
+sleep 2
