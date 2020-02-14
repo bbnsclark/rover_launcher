ce751dd5d72f1633d0b9e72aec3309247b313729
Giovani Del Nero Diniz <gdiniz@bbn.com>
2020-02-14 10:48:55 -0500

    [TEST] pc converter

--- bin/start.sh
+++ bin/start.sh
@@ -45,6 +45,11 @@ echo '    - video stream'
 screen -d -m -S video $HOME/rover/src/rover_launcher/bin/start_video_server.sh
 
 sleep 2
+echo '    - point cloud converter'
+
+screen -d -m -S pc_conv $HOME/rover/src/rover_launcher/bin/start_pc_converter.sh
+
+sleep 2
 echo '    - obstacle avoidance'
 
 screen -d -m -S avoidance $HOME/rover/src/rover_launcher/bin/start_avoidance.sh
