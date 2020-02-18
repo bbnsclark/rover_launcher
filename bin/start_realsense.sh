7af474d90861b88ba18490cec2612698b0d9a75a
Giovani Del Nero Diniz <gdiniz@bbn.com>
2020-02-18 08:01:24 -0500

    [TEST] test launch realsense in hardware

--- bin/start_realsense.sh
+++ bin/start_realsense.sh
@@ -4,5 +4,4 @@
 [ -f "$HOME/rover/devel_isolated/setup.sh" ] && source "$HOME/rover/devel_isolated/setup.sh"
 [ -f "/etc/default/ros" ] && source "/etc/default/ros"
 
-
-roslaunch $HOME/rover/src/rover_launcher/launch/realsense.launch
+rosrun realsense_driver node.py
