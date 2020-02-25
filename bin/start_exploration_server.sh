46d21de34936ce230e02030cdf42440e4ddc98dd
Giovani Del Nero Diniz <gdiniz@bbn.com>
2020-02-25 10:39:57 -0500

    [DEVEL] using frontier exploration to navigate in exploration mode inside

--- bin/start_exploration_server.sh
+++ bin/start_exploration_server.sh
@@ -5,4 +5,4 @@
 [ -f "$HOME/rover/devel_isolated/setup.sh" ] && source "$HOME/rover/devel_isolated/setup.sh"
 [ -f "/etc/default/ros" ] && source "/etc/default/ros"
 
-rosrun exploration_server node.py
+roslaunch exploration_server exploration.launch
