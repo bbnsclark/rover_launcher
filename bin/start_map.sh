151eb9cf73501a660c4a215bfa42ff9c93950c3b
Giovani Del Nero Diniz <gdiniz@bbn.com>
2020-02-25 13:45:21 -0500

    [FIX] gmapping launch script

--- bin/start_map.sh
+++ bin/start_map.sh
@@ -5,4 +5,4 @@
 [ -f "$HOME/rover/devel_isolated/setup.sh" ] && source "$HOME/rover/devel_isolated/setup.sh"
 [ -f "/etc/default/ros" ] && source "/etc/default/ros"
 
-roslaunch $HOME/rover/src/rover_launcher/launch/map_inertial.launch
+roslaunch $HOME/rover/src/rover_launcher/launch/map.launch
