ce95a6815d94a70b82d2ae8281c135ede71f3081
Giovani Del Nero Diniz <gdiniz@bbn.com>
2020-03-17 13:34:43 -0400

    [FIX] transferred throttle functionality to camera driver

--- bin/start.sh
+++ bin/start.sh
@@ -64,8 +64,3 @@ echo '    - rosbridge'
 
 screen -d -m -S rosbridge $HOME/rover/src/rover_launcher/bin/start_rosbridge.sh
 
-sleep 2
-echo '    - camera throttle'
-
-screen -d -m -S throttle $HOME/rover/src/rover_launcher/bin/start_throttle.sh
-
